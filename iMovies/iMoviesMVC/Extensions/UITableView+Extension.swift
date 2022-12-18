79f61181c454914ee23235e3f22e5e1e58f90c29
eyupyasuntimur <eyup.yasuntimur@rempeople.com>
2022-12-18 15:06:06 +0300

    UICollectionView and UITableView extensions added

---
+++ iMovies/iMoviesMVC/Extensions/UITableView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UITableView+Extension.swift
+//  iMovies
+//
+//  Created by Eyüp Yasuntimur on 18.12.2022.
+//
+
+import UIKit
+
+extension UITableView {
+    func registerCell<T: UITableViewCell>(type: T.Type) where T: Reuseable {
+        self.register(UINib(nibName: T.reuseIdentifier, bundle: nil), forCellReuseIdentifier: T.reuseIdentifier)
+    }
+
+    func dequeueReusableCell<T: UITableViewCell> (forIndexPath indexPath: IndexPath) -> T where T: Reuseable {
+        return dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as! T
+    }
+}
