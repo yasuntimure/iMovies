479d89a47c9caef1dcc5dbde8230dc17c8746ec5
eyupyasuntimur <eyup.yasuntimur@rempeople.com>
2023-01-06 10:01:44 +0300

    HomeInteractor added

--- iMovies/iMoviesVIPER/Scenes/Home/HomeInteractor.swift
+++ iMovies/iMoviesVIPER/Scenes/Home/HomeInteractor.swift
@@ -5,4 +5,25 @@
 //  Created by Eyüp Yasuntimur on 5.01.2023.
 //
 
-import Foundation
+import iMoviesAPI
+
+final class HomeInteractor {
+
+    @Published internal var movies: [Movie] = []
+
+    private let service: WebServiceProtocol
+
+    init(service: WebServiceProtocol) {
+        self.service = service
+    }
+
+    func searchReviews(for movie: String) {
+        service?.search(movie: movie, completion: { [weak self] (response) in
+            guard let response = response else {
+                return
+            }
+            self?.movies = response.map(MoviePresentation.init)
+        })
+    }
+
+}
